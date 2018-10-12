ce9d7d75504fb5236decc93129813ddd0e3df74e
Darien <djentwistle@DarienEntwistle-VMBP.local>
2018-10-12 13:49:17 -0400

    removed Google Chat and added Slack installation

--- MacProvison-SummerBuild.sh
+++ MacProvison-SummerBuild.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-echo  Macbook New Hire Provisoning - Mac OS High Sierra Summer Build
-
+echo  Macbook New Hire Provisoning - Mac OS High Sierra Halloween Build
+say Welcome!
+say Macbook Provisoning has started... standby..
 #Downloads Google File stream
 sleep 5s
 sudo cd Desktop/
@@ -10,11 +11,78 @@ sleep 10s
 hdiutil mount GoogleDriveFileStream.dmg; sudo installer -pkg /Volumes/Install\ Google\ Drive\ File\ Stream/GoogleDriveFileStream.pkg -target "/Volumes/Macintosh HD"; hdiutil unmount /Volumes/Install\ Google\ Drive\ File\ Stream/
 sleep 5s
 
-#Downloads Google chat
-sudo cd ~
-sudo curl -O https://dl.google.com/chat/latest/InstallHangoutsChat.dmg
-sleep 10s
+#Downloads & Installs Slack  - GITHUB source https://github.com/bwiessner/install_latest_slack_osx_app/blob/master/install_latest_slack_osx_app.sh
+#gets current logged in user
+consoleuser=$(ls -l /dev/console | cut -d " " -f4)
 
+APP_NAME="Slack.app"
+APP_PATH="/Applications/$APP_NAME"
+APP_VERSION_KEY="CFBundleShortVersionString"
+
+
+DOWNLOAD_URL="https://slack.com/ssb/download-osx"
+finalDownloadUrl=$(curl "$DOWNLOAD_URL" -s -L -I -o /dev/null -w '%{url_effective}')
+dmgName=$(printf "%s" "${finalDownloadUrl[@]}" | sed 's@.*/@@')
+slackDmgPath="/tmp/$dmgName"
+
+################################
+
+#find new version of Slack
+currentSlackVersion=$(/usr/bin/curl -s 'https://downloads.slack-edge.com/mac_releases/releases.json' | grep -o "[0-9]\.[0-9]\.[0-9]" | tail -1)
+
+if [ -d "$APP_PATH" ]; then
+    localSlackVersion=$(defaults read "$APP_PATH/Contents/Info.plist" "$APP_VERSION_KEY")
+    if [ "$currentSlackVersion" = "$localSlackVersion" ]; then
+        printf "Slack is already up-to-date. Version: %s" "$localSlackVersion"
+        exit 0
+    fi
+fi
+
+#find if slack is running
+if pgrep '[S]lack'; then
+    printf "Error: Slack is currently running!\n"
+    exit 409
+else
+
+# Remove the existing Application
+rm -rf /Applications/Slack.app
+
+#downloads latest version of Slack
+curl -L -o "$slackDmgPath" "$finalDownloadUrl"
+
+#mount the .dmg
+hdiutil attach -nobrowse $slackDmgPath
+
+#Copy the update app into applications folder
+sudo cp -R /Volumes/Slack*/Slack.app /Applications
+
+#unmount and eject dmg
+mountName=$(diskutil list | grep Slack | awk '{ print $3 }')
+umount -f /Volumes/Slack*/
+diskutil eject $mountName
+
+#clean up /tmp download
+rm -rf "$slackDmgPath"
+
+# Slack permissions are really dumb
+chown -R $consoleuser:admin "/Applications/Slack.app"
+
+localSlackVersion=$(defaults read "$APP_PATH/Contents/Info.plist" "$APP_VERSION_KEY")
+    if [ "$currentSlackVersion" = "$localSlackVersion" ]; then
+        printf "Slack is now updated/installed. Version: %s" "$localSlackVersion"
+    fi
+fi
+
+#slack will relaunch if it was previously running
+if [ "$slackOn" == "" ] ; then
+	exit 0
+else
+	su - "${consoleuser}" -c 'open -a /Applications/Slack.app'
+fi
+
+    exit 0
+fi
+sleep 2s
 #install user admin
 sudo installer -pkg create_uadmin-1.2.pkg -target "/Volumes/Macintosh HD"
 
