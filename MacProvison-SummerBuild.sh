94ecc7cd6a8fbd126cf107f4b3b01474e64b868b
Darien <djentwistle@DarienEntwistle-VMBP.local>
2018-10-12 14:01:48 -0400

    adjusted when slack is installed - fixed abrupt stop

--- MacProvison-SummerBuild.sh
+++ MacProvison-SummerBuild.sh
@@ -11,6 +11,60 @@ sleep 10s
 hdiutil mount GoogleDriveFileStream.dmg; sudo installer -pkg /Volumes/Install\ Google\ Drive\ File\ Stream/GoogleDriveFileStream.pkg -target "/Volumes/Macintosh HD"; hdiutil unmount /Volumes/Install\ Google\ Drive\ File\ Stream/
 sleep 5s
 
+#install user admin
+sudo installer -pkg create_uadmin-1.2.pkg -target "/Volumes/Macintosh HD"
+
+#Enable Firewall
+sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate on
+#allow Signed Apps on Firewall
+sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setallowsigned on
+
+
+#Disable Guest account
+sudo defaults write /Library/Preferences/com.apple.AppleFileServer guestAccess -bool NO
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server AllowGuestAccess -bool NO
+echo Disabled Guest account!
+#Disable animations when opening and closing windows.
+sudo defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
+# Disable animations when opening a Quick Look windowß
+sudo defaults write -g QLPanelAnimationDuration -float 0
+#Disable animation when opening the Info window in Finde
+sudo defaults write com.apple.finder DisableAllAnimations -bool true
+#Make all animations faster that are used by Mission Control
+sudo defaults write com.apple.dock expose-animation-duration -float 0.1
+
+#update & install Google Chrome
+dmgfile="googlechrome.dmg"
+volname="Google Chrome"
+logfile="/Library/Logs/GoogleChromeInstallScript.log"
+
+url='https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg'
+
+# Are we running on Intel?
+if [ '`/usr/bin/uname -p`'="i386" -o '`/usr/bin/uname -p`'="x86_64" ]; then
+		/bin/echo "--" >> ${logfile}
+		/bin/echo "`date`: Downloading latest version." >> ${logfile}
+		/usr/bin/curl -s -o /tmp/${dmgfile} ${url}
+		/bin/echo "`date`: Mounting installer disk image." >> ${logfile}
+		/usr/bin/hdiutil attach /tmp/${dmgfile} -nobrowse -quiet
+		/bin/echo "`date`: Installing..." >> ${logfile}
+		ditto -rsrc "/Volumes/${volname}/Google Chrome.app" "/Applications/Google Chrome.app"
+		/bin/sleep 10
+		/bin/echo "`date`: Unmounting installer disk image." >> ${logfile}
+		/usr/bin/hdiutil detach $(/bin/df | /usr/bin/grep "${volname}" | awk '{print $1}') -quiet
+		/bin/sleep 10
+		/bin/echo "`date`: Deleting disk image." >> ${logfile}
+		/bin/rm /tmp/"${dmgfile}"
+else
+	/bin/echo "`date`: ERROR: This script is for Intel Macs only." >> ${logfile}
+fi
+
+sleep 2s
+echo File Vault now being enabled!
+sudo fdesetup enable
+pause
+sleep 1s
+
 #Downloads & Installs Slack  - GITHUB source https://github.com/bwiessner/install_latest_slack_osx_app/blob/master/install_latest_slack_osx_app.sh
 #gets current logged in user
 consoleuser=$(ls -l /dev/console | cut -d " " -f4)
@@ -83,59 +137,8 @@ fi
     exit 0
 fi
 sleep 2s
-#install user admin
-sudo installer -pkg create_uadmin-1.2.pkg -target "/Volumes/Macintosh HD"
-
-#Enable Firewall
-sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate on
-#allow Signed Apps on Firewall
-sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setallowsigned on
 
 
-#Disable Guest account
-sudo defaults write /Library/Preferences/com.apple.AppleFileServer guestAccess -bool NO
-sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server AllowGuestAccess -bool NO
-echo Disabled Guest account!
-#Disable animations when opening and closing windows.
-sudo defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
-# Disable animations when opening a Quick Look windowß
-sudo defaults write -g QLPanelAnimationDuration -float 0
-#Disable animation when opening the Info window in Finde
-sudo defaults write com.apple.finder DisableAllAnimations -bool true
-#Make all animations faster that are used by Mission Control
-sudo defaults write com.apple.dock expose-animation-duration -float 0.1
-
-#update & install Google Chrome
-dmgfile="googlechrome.dmg"
-volname="Google Chrome"
-logfile="/Library/Logs/GoogleChromeInstallScript.log"
-
-url='https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg'
-
-# Are we running on Intel?
-if [ '`/usr/bin/uname -p`'="i386" -o '`/usr/bin/uname -p`'="x86_64" ]; then
-		/bin/echo "--" >> ${logfile}
-		/bin/echo "`date`: Downloading latest version." >> ${logfile}
-		/usr/bin/curl -s -o /tmp/${dmgfile} ${url}
-		/bin/echo "`date`: Mounting installer disk image." >> ${logfile}
-		/usr/bin/hdiutil attach /tmp/${dmgfile} -nobrowse -quiet
-		/bin/echo "`date`: Installing..." >> ${logfile}
-		ditto -rsrc "/Volumes/${volname}/Google Chrome.app" "/Applications/Google Chrome.app"
-		/bin/sleep 10
-		/bin/echo "`date`: Unmounting installer disk image." >> ${logfile}
-		/usr/bin/hdiutil detach $(/bin/df | /usr/bin/grep "${volname}" | awk '{print $1}') -quiet
-		/bin/sleep 10
-		/bin/echo "`date`: Deleting disk image." >> ${logfile}
-		/bin/rm /tmp/"${dmgfile}"
-else
-	/bin/echo "`date`: ERROR: This script is for Intel Macs only." >> ${logfile}
-fi
-
-sleep 2s
-echo File Vault now being enabled!
-sudo fdesetup enable
-pause
-sleep 1s
 echo  Provisioning has completed!
 say Provisioning has completed!
 echo GOODBYE!
